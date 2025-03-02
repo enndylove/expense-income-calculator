7b4695125ad0ec7330ca6602396b61f88bf99b9a
enndy <91780685+enndylove@users.noreply.github.com>
2025-03-02 13:18:55 +0200

    tanstack router implementation | tailwind and postcss configure

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import tailwindcss from '@tailwindcss/vite'
-
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
-    tailwindcss()
   ],
   server: {
     port: 3000,
