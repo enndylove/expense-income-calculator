2f5feccea2ddf7a01956debbc1771e489c5e7b27
enndy <91780685+enndylove@users.noreply.github.com>
2025-03-02 13:20:02 +0200

    update tailwind config, add to content ts and tsx

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js,ts,tsx}'],
   theme: {
     colors: {
       'blue': '#1fb6ff',
