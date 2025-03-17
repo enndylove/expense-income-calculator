5f58a9ac7f11deefc92125568b40555ad05d387b
enndy <91780685+enndylove@users.noreply.github.com>
2025-03-17 21:29:00 +0200

    edit auth guard component

--- client/src/shared/guards/AuthGuard.tsx
+++ client/src/shared/guards/AuthGuard.tsx
@@ -1,15 +1,7 @@
-import { useEffect } from "react";
 import { useAuth } from "../hooks/useAuth";
-import { redirect } from "@tanstack/react-router";
 
 export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
   const { isLoggedIn, isLoading } = useAuth();
-
-  useEffect(() => {
-    if (!isLoading && !isLoggedIn) {
-      redirect({ to: "/login" });
-    }
-  }, [isLoggedIn, isLoading]);
 
   if (isLoading) {
     return <div>Loading...</div>;
