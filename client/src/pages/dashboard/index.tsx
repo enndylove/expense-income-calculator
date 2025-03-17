8c0ed49bfd313402f3ba7eaa8f3f02eea5e56ef0
enndy <91780685+enndylove@users.noreply.github.com>
2025-03-17 20:28:58 +0200

    stating dashboard | header

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,5 +1,13 @@
+import { BackgroundGrid } from "@/shared/components/BackgroundGrid";
 import { AuthGuard } from "@/shared/guards/AuthGuard";
 
 export function DashboardComponent() {
-  return <AuthGuard>Hello</AuthGuard>;
+  return (
+    <AuthGuard>
+      <div className="flex flex-col max-w-4xl w-full mx-auto">
+        <BackgroundGrid />
+        <h1 className="text-4xl font-semibold tracking-tighter">Balance</h1>
+      </div>
+    </AuthGuard>
+  );
 }
--- client/src/shared/components/Header.tsx
+++ client/src/shared/components/Header.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "@tanstack/react-router";
+import { AuthGuard } from "../guards/AuthGuard";
+
 export function Header() {
-  return <header className="max-w-4xl m-[20px_auto_0px_auto]"></header>;
+  return (
+    <header className="flex items-center justify-between w-full max-w-4xl m-[20px_auto_0px_auto]">
+      <span>enndy</span>
+      <AuthGuard>
+        <Link to="/dashboard">
+          <Button
+            className="cursor-pointer"
+            variant={"neutralGhost"}
+            size={"sm"}
+          >
+            Dashboard
+          </Button>
+        </Link>
+      </AuthGuard>
+    </header>
+  );
 }
