6f2de5c117523f9a071b0a5e3de9b2e65eedcfdd
enndy <91780685+enndylove@users.noreply.github.com>
2025-04-24 13:29:22 +0300

    currencies constants | types

---
+++ client/src/shared/types/constants/currencies.type.ts
@@ -0,0 +1,14 @@
+export type CurrenciesType = {
+  name: string,
+  demonym: string,
+  majorSingle: string,
+  majorPlural: string,
+  ISOnum: number | null,
+  symbol: string,
+  symbolNative: string,
+  minorSingle: string,
+  minorPlural: string,
+  ISOdigits: number,
+  decimals: number,
+  numToBasic: number | null
+}
